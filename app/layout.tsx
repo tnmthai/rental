a99c15c1d9a34ff69484bb81b1373fe90b50d2c2
Thai Tran <tnmthai@gmail.com>
2026-05-16 15:39:56 +1200

    fix: use client component for Histats

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
 import Providers from './providers';
+import Histats from '../components/Histats';
 
 const SITE_URL = 'https://www.rentfinder.nz';
 
@@ -75,21 +75,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </footer>
           </div>
         </Providers>
-        {/* Histats.com START */}
-        <div id="histats_counter"></div>
-        <Script id="histats" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
-          var _Hasync= _Hasync|| [];
-          _Hasync.push(['Histats.start', '1,5027110,4,24,200,50,00011111']);
-          _Hasync.push(['Histats.fasi', '1']);
-          _Hasync.push(['Histats.track_hits', '']);
-          (function() {
-            var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-            hs.src = ('//s10.histats.com/js15_as.js');
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-          })();
-        ` }} />
-        <noscript><a href="/" target="_blank"><img src="//sstatic1.histats.com/0.gif?5027110&101" alt="" /></a></noscript>
-        {/* Histats.com END */}
+        <Histats />
       </body>
     </html>
   );
---
+++ components/Histats.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { useEffect } from 'react';
+
+export default function Histats() {
+  useEffect(() => {
+    // @ts-ignore
+    var _Hasync: any[] = (window as any)._Hasync = (window as any)._Hasync || [];
+    _Hasync.push(['Histats.start', '1,5027110,4,24,200,50,00011111']);
+    _Hasync.push(['Histats.fasi', '1']);
+    _Hasync.push(['Histats.track_hits', '']);
+    const hs = document.createElement('script');
+    hs.type = 'text/javascript';
+    hs.async = true;
+    hs.src = 'https://s10.histats.com/js15_as.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+  }, []);
+
+  return (
+    <>
+      <div id="histats_counter"></div>
+      <noscript>
+        <a href="/" target="_blank">
+          <img src="https://sstatic1.histats.com/0.gif?5027110&101" alt="" />
+        </a>
+      </noscript>
+    </>
+  );
+}
