c27048e4243c402b38a2bf8f54035c101151b9b9
Thai Tran <tnmthai@gmail.com>
2026-05-21 20:41:30 +1200

    Update Clarity tracking ID to wuhqi03cfc

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,7 +81,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "wrws151z0j");
+            })(window, document, "clarity", "script", "wuhqi03cfc");
           `}
         </Script>
       </body>
